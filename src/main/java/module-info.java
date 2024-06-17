fa56b25a7ae0ea89bb938c7fc409041d648975e8
LOQ-burh <nguyenminhquan042004@gmail.com>
2024-06-17 13:59:33 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.demo.mainapp.crossyroaddemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.demo.mainapp.crossyroaddemo to javafx.fxml;
+    exports com.demo.mainapp.crossyroaddemo;
+}
